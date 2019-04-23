f691cfa356ee6ac272ce49bf3b55020c731b3d14
huhuhang <huhuhang@users.noreply.github.com>
2019-04-23 09:47:37 +0800

    Update shiyanlou_user.py

--- Answers/week1-challenge-04/shiyanlou_user.py
+++ Answers/week1-challenge-04/shiyanlou_user.py
@@ -7,7 +7,7 @@ def user_info(user_id):
     url = "https://www.shiyanlou.com/users/{}/".format(user_id)
     content = requests.get(url)
 
-    try:
+    if content.status_code == 200:
         tree = html.fromstring(content.text)
         user_name = tree.xpath(
             '//div[@class="user-meta"]/span/text()')[0].strip()
@@ -16,6 +16,6 @@ def user_info(user_id):
         join_date = tree.xpath(
             '//span[@class="user-join-date"]/text()')[0].strip()[:10]
         return user_name, int(user_level), join_date
-    except ValueError:
+    else:
         user_name, user_level, join_date = (None, None, None)
         return user_name, user_level, join_date
