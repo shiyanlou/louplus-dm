64656dbf18d9ebcb23788132027d1a1014369a0a
huhuhang <huhuhang@gmail.com>
2019-08-05 16:39:32 +0800

    fix bugs

--- Answers/week2-challenge-02/earthquake.py
+++ Answers/week2-challenge-02/earthquake.py
@@ -2,16 +2,19 @@ import pandas as pd
 
 
 def clean():
-    # 读取数据
+    # 读取据
     df = pd.read_csv("earthquake.csv")
     # 选择需保留列
     df1 = df[['time', 'latitude', 'longitude', 'depth', 'mag']]
-    # 对 place 列使用自定义分割，得到需要的 region 数据
-    df2 = pd.DataFrame(df.place.str.split(', ', 1).tolist(),
-                       columns=['address', 'region'])['region']
+    # 对 place 列使用分割，得到需要的 region 数据
+    place = df.place.str.split(', ').tolist()
+    region = []
+    for row in place:
+        region.append(row[-1])
+    df2 = pd.DataFrame(region, columns=['region'])
     # 拼接数据
     df = pd.concat([df1, df2], axis=1)
-    # 去除重复值和缺失值行
+    # 去除重复值
     df_clean = df.drop_duplicates().dropna()
-    
+
     return df_clean
